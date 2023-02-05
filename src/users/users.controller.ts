77de8f4972cb3e375f5c71a10a9540c1b7eacf8e
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2023-02-05 18:29:57 +0400

    feat: implement module for manage users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,40 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { UserDto } from './dto/user.dto';
+import { Delete, HttpCode } from '@nestjs/common/decorators';
 
 @Controller('user')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
+  createUser(@Body() createUserDto: CreateUserDto): UserDto {
     return this.usersService.createUser(createUserDto);
   }
 
   @Get()
-  getUsers(): User[] {
+  getUsers(): UserDto[] {
     return this.usersService.getUsers();
   }
 
   @Get(':id')
-  getUser(@Param('id') id: string) {
+  getUser(@Param('id') id: string): UserDto {
     return this.usersService.getUser(id);
+  }
+
+  @Put(':id')
+  updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ): UserDto {
+    return this.usersService.updatePassword(id, updatePasswordDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,31 +1,119 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './interfaces/user.interface';
+import { v4 as uuidv4, validate as validateUUID } from 'uuid';
+import { UserDto } from './dto/user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
 
 @Injectable()
 export class UsersService {
+  // TODO: mb after adding DB write users to this array
   private readonly users: User[] = [];
 
-  createUser(createUserdto: CreateUserDto): User {
-    // TODO:fix adding fields for user, do some actions with password
+  // TODO: mb move to utils
+  private getUserDto(user: User): UserDto {
+    const { createdAt, id, login, updatedAt, version } = user;
+    return { createdAt, id, login, updatedAt, version };
+  }
+
+  createUser(createUserDto: CreateUserDto): UserDto {
+    // TODO: mb move validate 'createUserdto' to utils
+    const isLoginString = typeof createUserDto.login === 'string';
+    const isPasswordString = typeof createUserDto.password === 'string';
+    const isCreateUserDtoValid = isLoginString && isPasswordString;
+
+    if (!isCreateUserDtoValid) {
+      throw new BadRequestException(
+        'Body does not contain required fields or fields are of the wrong type',
+      );
+    }
+
+    const date = new Date().getTime();
+
+    // TODO:move to utils
     const user: User = {
-      ...createUserdto,
-      id: '111',
-      createdAt: 123,
-      updatedAt: 123,
+      ...createUserDto,
+      id: uuidv4(),
+      createdAt: date,
+      updatedAt: date,
       version: 1,
     };
 
     this.users.push(user);
 
-    return user;
+    return this.getUserDto(user);
   }
 
-  getUsers(): User[] {
-    return this.users;
+  getUsers(): UserDto[] {
+    const userDtoList = this.users.map((user) => this.getUserDto(user));
+    return userDtoList;
   }
 
-  getUser(id: string): User {
-    return this.users.find((user) => user.id === id);
+  getUser(id: string): UserDto {
+    const isIdValid = validateUUID(id);
+
+    if (!isIdValid) {
+      throw new BadRequestException('User id is invalid (not uuid)');
+    }
+
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new NotFoundException("User doesn't exist");
+    }
+
+    return this.getUserDto(user);
+  }
+
+  updatePassword(id: string, updatePasswordDto: UpdatePasswordDto): UserDto {
+    // TODO: use fragment for dont repeat this block in deleteUser method
+    const isIdValid = validateUUID(id);
+
+    if (!isIdValid) {
+      throw new BadRequestException('User id is invalid (not uuid)');
+    }
+
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    const user = this.users[userIndex];
+
+    if (!user) {
+      throw new NotFoundException("User doesn't exist");
+    }
+
+    const isPasswordCorrect = user.password === updatePasswordDto.oldPassword;
+
+    if (!isPasswordCorrect) {
+      throw new NotFoundException('Old password is wrong');
+    }
+
+    const updatedUser: User = {
+      ...user,
+      password: updatePasswordDto.newPassword,
+    };
+
+    this.users[userIndex] = updatedUser;
+
+    return this.getUserDto(user);
+  }
+
+  deleteUser(id: string) {
+    const isIdValid = validateUUID(id);
+
+    if (!isIdValid) {
+      throw new BadRequestException('User id is invalid (not uuid)');
+    }
+
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    const user = this.users[userIndex];
+
+    if (!user) {
+      throw new NotFoundException("User doesn't exist");
+    }
+
+    this.users.splice(userIndex, 1);
   }
 }
